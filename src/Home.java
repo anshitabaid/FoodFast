a790d429f7c2dd5ad9ede8c3bc2dd5827759f737
anshitabaid <anshita_b@yahoo.co.in>
2019-03-13 11:15:46 +0530

    basic sql works

--- src/Home.java
+++ src/Home.java
@@ -10,11 +10,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
 
 public class Home extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtUID;
+	private JTextField txtPhno;
 	private JPasswordField txtPassword;
 
 	/**
@@ -44,57 +45,66 @@ public class Home extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(12, 24, 426, 271);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
-		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(154, 12, 158, 25);
-		panel.add(lblUsername);
-		
+
+		JLabel lblPhno = new JLabel("Phone Number");
+		lblPhno.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblPhno.setBounds(154, 12, 158, 25);
+		panel.add(lblPhno);
+
 		txtPassword = new JPasswordField();
 		txtPassword.setBounds(154, 105, 117, 19);
 		panel.add(txtPassword);
+
+		txtPhno = new JTextField();
+		txtPhno.setBounds(154, 49, 114, 19);
+		panel.add(txtPhno);
+		txtPhno.setColumns(10);
+
+		JButton btnSearch = new JButton("Login");
+
+		btnSearch.setBounds(154, 171, 117, 25);
+		panel.add(btnSearch);
+
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(154, 107, 133, 17);
+		panel.add(lblPassword);
 		
-		txtUID = new JTextField();
-		txtUID.setBounds(154, 49, 114, 19);
-		panel.add(txtUID);
-		txtUID.setColumns(10);
-		
-			JButton btnSearch = new JButton("Login");
-			
-					btnSearch.setBounds(154, 171, 117, 25);
-					panel.add(btnSearch);
-					
-					JLabel lblPassword = new JLabel("Password");
-					lblPassword.setBounds(154, 107, 133, 17);
-					panel.add(lblPassword);
+		JLabel lblData = new JLabel("");
+		lblData.setBounds(79, 214, 297, 25);
+		panel.add(lblData);
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					String uid = txtUID.getText();
-					String password = txtPassword.getText ();
-					//Get driver class
+					String phno = txtPhno.getText();
+					String password = String.valueOf(txtPassword.getPassword());
+					// Get driver class
 					Class.forName("oracle.jdbc.driver.OracleDriver");
-					//step2 create the connection object			
-			        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM", "16181618");
-			        //create statement
-			        Statement stmt = con.createStatement();
-			        ResultSet rs = stmt.executeQuery("SELECT * FROM users where uid = '" );
-			        
-			        con.commit();
-			        con.close();  
-			        
-			        	
+					// step2 create the connection object
+					Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "SYSTEM",
+							"16181618");
+					// create statement
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT * FROM users where phone_num = "+phno);
+					while (rs.next ()) {
+						System.out.println ("Not empty");
+						System.out.println (rs.getString("name"));
+						//lblData.setText(rs.getString("phone_num") + " " + rs.getString ("name"));
+					}
+					
+
+					con.commit();
+					con.close();
+
 				} catch (Exception e) {
 					e.printStackTrace();
-				}			
-		        
+				}
+
 			}
 		});
-		
-	
-	}
-}
+
+}}
